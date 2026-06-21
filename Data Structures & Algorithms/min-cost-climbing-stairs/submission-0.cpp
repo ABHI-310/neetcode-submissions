f1a0611e4e131c96ffa766783af2f57c5af55c0c
Abhinav Gupta <58878573+ABHI-310@users.noreply.github.com>
2026-06-22 00:53:41 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> t(n+1);
+
+        for(int i=2;i<=n;i++){
+            t[i]=min(t[i-1]+cost[i-1], t[i-2]+cost[i-2]);
+        }
+
+        return t[n];
+    }
+};
